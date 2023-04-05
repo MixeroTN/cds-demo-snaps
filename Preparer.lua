0e28db0920ec8075c8e4cc4c27640053bdb01674
MixeroTN <40803091+MixeroTN@users.noreply.github.com>
2023-04-05 00:00:38 +0000

    Create Preparer.lua

---
+++ Preparer.lua
@@ -0,0 +1,47 @@
+--ReplicatedFirst/Preparer.lua
+local ContentProvider = game:GetService("ContentProvider")
+local Lighting = game:GetService("Lighting")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local Intro = script.Parent:WaitForChild("Intro")
+    local LaunchInStudio = Intro:WaitForChild("LaunchInStudio")
+local PlayerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
+    local IntroGUI = PlayerGui:WaitForChild("IntroGUI")
+local L_White = Lighting:WaitForChild("White")
+
+local toPreload = {
+    Lighting,
+    PlayerGui,
+    ReplicatedStorage
+}
+
+
+function environmentCheck()
+    return RunService:IsStudio() and not LaunchInStudio.Value
+end
+
+function preloadAsync(...)
+    for _, object in ipairs({...}) do
+        pcall(function()
+            ContentProvider:PreloadAsync(object)
+        end)
+    end
+end
+
+function prepareObjects()
+    L_White.Enabled = true
+end
+
+function skipIntro()
+    if environmentCheck() then
+        IntroGUI.Event:Fire()
+        return true
+    end
+end
+
+
+skipIntro()
+preloadAsync(toPreload)
+prepareObjects()
